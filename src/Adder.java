6b856d5b016beedfffcd99c11146e708392b3a92
qkfns <qkfns@naver.com>
2020-11-17 17:58:55 +0900

    트라이 캐치 연습중

--- src/Adder.java
+++ src/Adder.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class Adder {
     public static void main(String[] args) {
-        int num1,num2;
-
+        int num1,num2,sum;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("숫자를 입력하세요.");
-        num1 = scanner.nextInt();
-        System.out.println("숫자를 하나더 입력하세요.");
-        num2 = scanner.nextInt();
 
-        System.out.println("입력한 값을 더한 값 = " + (num1+num2));
+        try{System.out.println("숫자를 입력하세요.");
+            num1 = scanner.nextInt();
+            System.out.println("숫자를 하나더 입력하세요.");
+            num2 = scanner.nextInt();
+            sum = num1+num2;
+            System.out.println("입력한 값을 더한 값 = " + (sum));
+
+        }catch (Exception e){
+            System.out.println("숫자가 아닙니다. 오류가 발생했습니다.");
+
+        }
     }
-
 }
--- src/DogFood.java
+++ src/DogFood.java
@@ -7,10 +7,14 @@ public class DogFood {
         int DogKg;
         Scanner scanner = new Scanner(System.in);
 
+        try {
+            System.out.println("강아지 몸무게를 입력해주세요.");
+            DogKg = scanner.nextInt();
+            System.out.println("1일 권장 급여량: " + (double) DogKg * kg / g + "g");
+        }catch(Exception e){
+            System.out.println("오류입니다.");
+        }
 
-        System.out.println("강아지 몸무게를 입력해주세요.");
-        DogKg = scanner.nextInt();
-        System.out.println("1일 권장 급여량: " + (double)DogKg*kg/g + "g");
 
     }
 }
